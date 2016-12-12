c42c404b275bc20e3eb773ecae84eab64cfaa80f
Ragha <ragha@outlook.com>
2016-12-12 01:24:28 -0800

    updated example

--- examples/visualize_filters.py
+++ examples/visualize_filters.py
@@ -17,7 +17,7 @@ def main():
 
     # the name of the layer we want to visualize
     # (see model definition at keras/applications/vgg16.py)
-    layer_name = 'block2_conv2'
+    layer_name = 'block5_conv2'
 
     for filter_idx in range(10):
         regularizers = [(TotalVariation(), 0.01)]
@@ -25,7 +25,7 @@ def main():
         opt = Optimizer(model, regularizers, losses, max_iter=200,
                         layer_name=layer_name, filter_idx=filter_idx)
         img = opt.minimize()
-        cv2.imshow('filter_i'.format(filter_idx), img)
+        cv2.imshow('filter_{}'.format(filter_idx), img)
         cv2.waitKey(0)
 
 if __name__ == '__main__':
