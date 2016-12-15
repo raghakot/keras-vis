e5d750a518b52b708630c790fd20ce1e5dd55d42
Ragha <ragha@outlook.com>
2016-12-14 18:12:22 -0800

    Adding util to create stitched images, get imagenet label names.

--- utils.py
+++ utils.py
@@ -1,26 +1,10 @@
+from __future__ import division
+
 import numpy as np
-import cv2
+import os
+import math
+import json
 from keras import backend as K
-
-
-def save_filters(filters, img_width, img_height):
-    margin = 5
-    n = int(len(filters)**0.5)
-    width = n * img_width + (n - 1) * margin
-    height = n * img_height + (n - 1) * margin
-    stitched_filters = np.zeros((width, height, 3))
-
-    # fill the picture with our saved filters
-    for i in range(n):
-        for j in range(n):
-            index = i * n + j
-            if index < len(filters):
-                img = filters[i * n + j]
-                stitched_filters[(img_width + margin) * i: (img_width + margin) * i + img_width,
-                (img_height + margin) * j: (img_height + margin) * j + img_height, :] = img
-
-    # save the result to disk
-    cv2.imwrite('stitched_filters_%dx%d.png' % (n, n), stitched_filters)
 
 
 # util function to convert a tensor into a valid image
@@ -47,6 +31,36 @@ def deprocess_image(x):
 def normalize(x):
     # utility function to normalize a tensor by its L2 norm
     return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
+
+
+def stitch_images(images, margin=5, cols=5):
+    """
+    Util to stitch images together. Will fold over images to the next row when max_width is exceeded.
+    :param images: The images to stitch
+    :param margin: The black border margin size between images
+    :param cols: Max number of cols to show. New rows are created when max col limit is exceeded.
+    """
+    if len(images) == 0:
+        return None
+
+    h, w, c = images[0].shape
+    n_rows = int(math.ceil(len(images) / cols))
+    n_cols = min(len(images), cols)
+
+    out_w = n_cols * w + (n_cols - 1) * margin
+    out_h = n_rows * h + (n_rows - 1) * margin
+    stitched_images = np.zeros((out_h, out_w, c), dtype=images[0].dtype)
+
+    for row in range(n_rows):
+        for col in range(n_cols):
+            img_idx = row * cols + col
+            if img_idx >= len(images):
+                break
+
+            stitched_images[(h + margin) * row : (h + margin) * row + h,
+            (w + margin) * col : (w + margin) * col + w, :] = images[img_idx]
+
+    return stitched_images
 
 
 def generate_rand_img(c, w, h):
@@ -80,6 +94,15 @@ def get_image_indices():
         return 0, 1, 2, 3
     else:
         return 0, 3, 1, 2
+
+
+CLASS_INDEX = None
+def get_imagenet_label(index):
+    global CLASS_INDEX
+    if CLASS_INDEX is None:
+        with open(os.path.join(os.path.dirname(__file__), 'resources/imagenet_class_index.json')) as f:
+            CLASS_INDEX = json.load(f)
+    return CLASS_INDEX[str(index)][1]
 
 
 class BackendAgnosticImageSlice(object):
