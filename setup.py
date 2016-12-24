bafc072dd4c5e7a128ed5fde4897aa3366f3c319
Ragha <ragha@outlook.com>
2016-12-23 17:41:45 -0800

    fix download url

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+version = '0.1.1'
+
 setup(name='keras-vis',
-      version='0.1.1',
-      description='Network visualization toolkit for keras',
+      version=version,
+      description='Neural Network visualization toolkit for keras',
       author='Raghavendra Kotikalapudi',
       author_email='ragha@outlook.com',
       url='https://github.com/raghakot/keras-vis',
-      download_url='https://github.com/raghakot/keras-vis/tarball/0.1',
+      download_url='https://github.com/raghakot/keras-vis/tarball/{}'.format(version),
       license='MIT',
       install_requires=['keras', 'imageio'],
       packages=find_packages())
