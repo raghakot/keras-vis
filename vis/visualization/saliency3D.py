251b3d0a27646783114f47f598225772e7d16cd7
kitamura-felipe <kitamura.felipe@gmail.com>
2018-07-21 00:29:57 -0300

    Update saliency3D.py

--- vis/visualization/saliency3D.py
+++ vis/visualization/saliency3D.py
@@ -46,87 +46,6 @@ def _find_penultimate_layer(model, layer_idx, penultimate_layer_idx):
 
     return model.layers[penultimate_layer_idx]
 
-
-def visualize_saliency_with_losses(input_tensor, losses, seed_input, grad_modifier='absolute'):
-    """Generates an attention heatmap over the `seed_input` by using positive gradients of `input_tensor`
-    with respect to weighted `losses`.
-
-    This function is intended for advanced use cases where a custom loss is desired. For common use cases,
-    refer to `visualize_class_saliency` or `visualize_regression_saliency`.
-
-    For a full description of saliency, see the paper:
-    [Deep Inside Convolutional Networks: Visualising Image Classification Models and Saliency Maps]
-    (https://arxiv.org/pdf/1312.6034v2.pdf)
-
-    Args:
-        input_tensor: An input tensor of shape: `(samples, channels, image_dims...)` if `image_data_format=
-            channels_first` or `(samples, image_dims..., channels)` if `image_data_format=channels_last`.
-        losses: List of ([Loss](vis.losses#Loss), weight) tuples.
-        seed_input: The model input for which activation map needs to be visualized.
-        grad_modifier: gradient modifier to use. See [grad_modifiers](vis.grad_modifiers.md). By default `absolute`
-            value of gradients are used. To visualize positive or negative gradients, use `relu` and `negate`
-            respectively. (Default value = 'absolute')
-
-    Returns:
-        The heatmap image indicating the `seed_input` regions whose change would most contribute towards minimizing
-        weighted `losses`.
-    """
-    opt = Optimizer(input_tensor, losses, norm_grads=False)
-    grads = opt.minimize(seed_input=seed_input, max_iter=1, grad_modifier=grad_modifier, verbose=False)[1]
-
-    channel_idx = 1 if K.image_data_format() == 'channels_first' else -1
-    grads = np.max(grads, axis=channel_idx)
-
-    # Normalize and create heatmap.
-    grads = utils.normalize(grads)
-    return np.uint8(cm.jet(grads)[..., :3] * 255)[0]
-
-
-def visualize_saliency(model, layer_idx, filter_indices, seed_input,
-                       backprop_modifier=None, grad_modifier='absolute'):
-    """Generates an attention heatmap over the `seed_input` for maximizing `filter_indices`
-    output in the given `layer_idx`.
-
-    Args:
-        model: The `keras.models.Model` instance. The model input shape must be: `(samples, channels, image_dims...)`
-            if `image_data_format=channels_first` or `(samples, image_dims..., channels)` if
-            `image_data_format=channels_last`.
-        layer_idx: The layer index within `model.layers` whose filters needs to be visualized.
-        filter_indices: filter indices within the layer to be maximized.
-            If None, all filters are visualized. (Default value = None)
-            For `keras.layers.Dense` layer, `filter_idx` is interpreted as the output index.
-            If you are visualizing final `keras.layers.Dense` layer, consider switching 'softmax' activation for
-            'linear' using [utils.apply_modifications](vis.utils.utils#apply_modifications) for better results.
-        seed_input: The model input for which activation map needs to be visualized.
-        backprop_modifier: backprop modifier to use. See [backprop_modifiers](vis.backprop_modifiers.md). If you don't
-            specify anything, no backprop modification is applied. (Default value = None)
-        grad_modifier: gradient modifier to use. See [grad_modifiers](vis.grad_modifiers.md). By default `absolute`
-            value of gradients are used. To visualize positive or negative gradients, use `relu` and `negate`
-            respectively. (Default value = 'absolute')
-
-    Example:
-        If you wanted to visualize attention over 'bird' category, say output index 22 on the
-        final `keras.layers.Dense` layer, then, `filter_indices = [22]`, `layer = dense_layer`.
-
-        One could also set filter indices to more than one value. For example, `filter_indices = [22, 23]` should
-        (hopefully) show attention map that corresponds to both 22, 23 output categories.
-
-    Returns:
-        The heatmap image indicating the `seed_input` regions whose change would most contribute towards
-        maximizing the output of `filter_indices`.
-    """
-    if backprop_modifier is not None:
-        modifier_fn = get(backprop_modifier)
-        model = modifier_fn(model)
-
-    # `ActivationMaximization` loss reduces as outputs get large, hence negative gradients indicate the direction
-    # for increasing activations. Multiply with -1 so that positive gradients indicate increase instead.
-    losses = [
-        (ActivationMaximization(model.layers[layer_idx], filter_indices), -1)
-    ]
-    return visualize_saliency_with_losses(model.input, losses, seed_input, grad_modifier)
-
-
 def visualize_cam_with_losses(input_tensor, losses,
                               seed_input, penultimate_layer,
                               grad_modifier=None):
@@ -202,7 +121,7 @@ def visualize_cam_with_losses(input_tensor, losses,
     for i in range(len(mapa_ex)):
         mapa_ex2[i, :, :] = cv2.resize(mapa_ex[i, :, :],(target_size[2], target_size[1]), interpolation = cv2.INTER_CUBIC)	
     	
-    return mapa_ex2 #np.uint8(cm.jet(heatmap)[..., :] * 255) #3
+    return mapa_ex2
 
 
 def visualize_cam(model, layer_idx, filter_indices,
