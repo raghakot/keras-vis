6fc8f9fa4b19b84cf3247bd068f20c7c690b6c87
keisen <k.keisen@gmail.com>
2018-08-14 07:05:37 +0000

    Improve existing test

--- tests/vis/test_optimizer.py
+++ tests/vis/test_optimizer.py
@@ -1,6 +1,6 @@
 import keras.backend as K
-from keras.layers import Dense
-from keras.models import Sequential
+from keras.layers import Dense, Input, Add
+from keras.models import Model, Sequential
 from vis.losses import Loss
 from vis.optimizer import Optimizer
 
@@ -19,6 +19,18 @@ class _DummyLoss(Loss):
 @pytest.fixture(scope="function", autouse=True)
 def model_and_losses():
     model = Sequential([Dense(4, activation='linear', input_shape=(2, ))])
+    losses = [(_DummyLoss(model), 1)]
+    return model, losses
+
+
+@pytest.fixture(scope="function", autouse=True)
+def multi_inputs_model_and_losses():
+    input_a = Input((2, ))
+    x1 = Dense(4, activation='linear')(input_a)
+    input_b = Input((2, ))
+    x2 = Dense(4, activation='linear')(input_b)
+    output = Add()([x1, x2])
+    model = Model(inputs=[input_a, input_b], outputs=output)
     losses = [(_DummyLoss(model), 1)]
     return model, losses
 
@@ -49,6 +61,16 @@ def test_wrt_tensors_isnt_input_tensor(model_and_losses):
     opt.minimize(max_iter=1)
 
     assert not opt.wrt_tensors_is_input_tensors
+    assert opt.wrt_tensors is not None
+    assert opt.wrt_tensors != opt.input_tensors
+
+
+def test_multi_inputs(multi_inputs_model_and_losses):
+    model, losses = multi_inputs_model_and_losses
+    opt = Optimizer(model.inputs, losses, wrt_tensors=None)
+    opt.minimize(max_iter=1)
+
+    assert opt.wrt_tensors_is_input_tensors
     assert opt.wrt_tensors is not None
     assert opt.wrt_tensors != opt.input_tensors
 
